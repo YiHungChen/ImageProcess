9cbe299b9f534aea435a034f4508bce148be39ec
YiHungChen <j12102003@hotmail.com>
2016-09-15 19:29:10 +0800

    RGB to GrayLevel
    inverse the original picture from RGB to grayLevel

--- Untitled.m
+++ Untitled.m
@@ -2,4 +2,5 @@
 %{ This is a program for testing the performers for githubs}%
 
 img.input = imread('D:\ConvertFrames\005\20160531avi (2016-5-31 ¤W¤È 11-17-18)\20160531avi 00001.jpg');
-imshow(img.input)
+image.input = rgb2gray(img.input);
+imshow(img.input);
