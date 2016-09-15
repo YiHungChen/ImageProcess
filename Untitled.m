aa1667dd8761ea41b3fc27dd285e569b0c3bb6e4
YiHungChen <j12102003@hotmail.com>
2016-09-15 19:53:11 +0800

    End of the program

--- Untitled.m
+++ Untitled.m
@@ -4,3 +4,6 @@
 img.input = imread('D:\ConvertFrames\005\20160531avi (2016-5-31 ¤W¤È 11-17-18)\20160531avi 00001.jpg');
 img.input = rgb2gray(img.input);
 imshow(img.input);
+
+%% End of the program
+
