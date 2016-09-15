607bd9a16647cba9fea38c834953c91441a3848b
YiHungChen <j12102003@hotmail.com>
2016-09-15 19:27:23 +0800

    Open New file
    Set up the first file for image reading

---
+++ Untitled.m
@@ -0,0 +1,5 @@
+%% Basic GitHub Test %%
+%{ This is a program for testing the performers for githubs}%
+
+img.input = imread('D:\ConvertFrames\005\20160531avi (2016-5-31 ¤W¤È 11-17-18)\20160531avi 00001.jpg');
+imshow(img.input)
